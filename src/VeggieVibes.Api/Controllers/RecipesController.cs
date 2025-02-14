ce981e2f15a0cff3a66c436a5c7c94a47479d307
Mariana Vasconcelos <vasconcelosmariana05@gmail.com>
2025-02-14 19:51:10 -0300

    add endpoint search all recipes

--- src/VeggieVibes.Api/Controllers/RecipesController.cs
+++ src/VeggieVibes.Api/Controllers/RecipesController.cs
@@ -11,11 +11,9 @@ namespace VeggieVibes.Api.Controllers;
 public class RecipesController : ControllerBase
 {
     [HttpPost]
-    [ProducesResponseType(typeof(ResponseRegisterRecipesJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseRegisteredIngredientsJson), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Register(
-        [FromServices] IRegisterRecipesUseCase useCase,
-        [FromBody] RequestRegisterRecipesJson request)
+    public async Task<IActionResult> Register([FromServices] IRegisteredRecipesUseCase useCase, [FromBody] RequestRegisterRecipesJson request)
     {
         var response = await useCase.Execute(request);
 
@@ -25,9 +23,7 @@ public class RecipesController : ControllerBase
     [HttpGet("{id}")]
     [ProducesResponseType(typeof(ResponseGetRecipeByIdJson), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetById(
-        [FromRoute] long id,
-        [FromServices] IGetRecipeByIdUseCase useCase)
+    public async Task<IActionResult> GetById([FromRoute] long id, [FromServices] IGetRecipeByIdUseCase useCase)
     {
         var response = await useCase.Execute(id);
 
@@ -35,5 +31,17 @@ public class RecipesController : ControllerBase
             return NotFound();
 
         return Ok(response);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseRecipesJson), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllRecipes([FromServices] IGetAllRecipesUseCase useCase)
+    {
+        var response = await useCase.Execute();
+
+        if (response.Recipes.Count != 0)
+            return Ok(response);
+
+        return NoContent();
     }
 }
