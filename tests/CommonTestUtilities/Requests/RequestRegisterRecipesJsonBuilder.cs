a597c8d88bb4dd5ddbdd83bf3ec5abe47c403f4f
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-03-12 07:07:32 -0300

    add fake data generator using bogus for RequestRegisterRecipesJson validation tests

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterRecipesJsonBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using VeggieVibes.Communication.Enums;
+using VeggieVibes.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterRecipesJsonBuilder
+    {
+        public static RequestRegisterRecipesJson Build()
+        {
+            var ingredientFaker = new Faker<RequestRegisterIngredientsJson>()
+            .RuleFor(i => i.Name, f => f.Commerce.ProductName()) 
+            .RuleFor(i => i.Quantity, f => f.Random.Decimal(0.1m, 5.0m)) 
+            .RuleFor(i => i.UnitOfMeasure, f => f.Random.Int(1, 5)); 
+
+            var recipeFaker = new Faker<RequestRegisterRecipesJson>()
+                .RuleFor(r => r.RecipeId, f => f.Random.Int(1, 1000))
+                .RuleFor(r => r.Title, f => f.Lorem.Word())
+                .RuleFor(r => r.Description, f => f.Lorem.Sentence())
+                .RuleFor(r => r.Ingredients, f => ingredientFaker.Generate(5)) 
+                .RuleFor(r => r.Category, f => f.PickRandom<RecipeCategory>())
+                .RuleFor(r => r.Difficulty, f => f.PickRandom<RecipeDifficulty>())
+                .RuleFor(r => r.DietType, f => f.PickRandom<DietType>())
+                .RuleFor(r => r.MealType, f => f.PickRandom<MealType>())
+                .RuleFor(r => r.Origin, f => f.PickRandom<CulinaryOrigin>())
+                .RuleFor(r => r.Instructions, f => f.Make(3, () => f.Lorem.Sentence())) 
+                .RuleFor(r => r.PreparationTimeMinutes, f => f.Random.Int(5, 60))
+                .RuleFor(r => r.CookingTimeMinutes, f => f.Random.Int(10, 90))
+                .RuleFor(r => r.CaloriesPerServing, f => f.Random.Int(100, 800))
+                .RuleFor(r => r.Allergen, f => f.PickRandom<Allergen>())
+                .RuleFor(r => r.MainImageUrl, f => f.Internet.Url())
+                .RuleFor(r => r.AdditionalImageUrls, f => f.Make(2, () => f.Internet.Url())) 
+                .RuleFor(r => r.Variations, f => f.Make(2, () => f.Lorem.Sentence()))
+                .RuleFor(r => r.SubstituteIngredients, f => f.Make(2, () => f.Lorem.Word()));
+
+            return recipeFaker.Generate();
+        }
+    }
+}
