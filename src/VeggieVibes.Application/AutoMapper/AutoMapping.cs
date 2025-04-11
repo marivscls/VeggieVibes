7183e00248aa478298c8bacfc7801627a0642906
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-11 06:20:02 -0300

    add config auto mapping

--- src/VeggieVibes.Application/AutoMapper/AutoMapping.cs
+++ src/VeggieVibes.Application/AutoMapper/AutoMapping.cs
@@ -18,33 +18,17 @@ public class AutoMapping : Profile
         CreateMap<RequestRecipeJson, Recipe>()
             .ForMember(dest => dest.Instructions, opt => opt.MapFrom(src => src.Instructions.Select(instruction => new Instruction { Step = instruction })))
             .ForMember(dest => dest.Variations, opt => opt.MapFrom(src => src.Variations.Select(variation => new Variation { Description = variation })))
-            .ForMember(dest => dest.SubstituteIngredients, opt => opt.MapFrom(src => src.SubstituteIngredients.Select(substitute => new SubstituteIngredient {OriginalIngredient = substitute,Substitute = substitute})))
-            .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src =>src.Ingredients.Select(ingredient => new RecipeIngredient{Ingredient = new Ingredient { Name = ingredient.Name },Quantity = ingredient.Quantity,UnitOfMeasure = (VeggieVibes.Domain.Enums.UnitOfMeasure)ingredient.UnitOfMeasure})))
-            .ForMember(dest => dest.Category, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.RecipeCategory)src.Category))
-            .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.RecipeDifficulty)src.Difficulty))
-            .ForMember(dest => dest.DietType, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.DietType)src.DietType))
-            .ForMember(dest => dest.MealType, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.MealType)src.MealType))
-            .ForMember(dest => dest.Origin, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.CulinaryOrigin)src.Origin))
-            .ForMember(dest => dest.Allergen, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.Allergen)src.Allergen))
-            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+            .ForMember(dest => dest.SubstituteIngredients, opt => opt.MapFrom(src => src.SubstituteIngredients.Select(substitute => new SubstituteIngredient { OriginalIngredient = substitute, Substitute = substitute })))
+            .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.Ingredients.Select(ingredient => new RecipeIngredient { Ingredient = new Ingredient { Name = ingredient.Name }, Quantity = ingredient.Quantity, UnitOfMeasure = (VeggieVibes.Domain.Enums.UnitOfMeasure)ingredient.UnitOfMeasure })));
 
         CreateMap<RequestUpdateRecipeJson, Recipe>()
-            .ForMember(dest => dest.Instructions, opt => opt.MapFrom(src =>src.Instructions.Select(instruction => new Instruction { Step = instruction }).ToList()))
+            .ForMember(dest => dest.Instructions, opt => opt.MapFrom(src => src.Instructions.Select(instruction => new Instruction { Step = instruction }).ToList()))
             .ForMember(dest => dest.Variations, opt => opt.MapFrom(src => src.Variations.Select(variation => new Variation { Description = variation }).ToList()))
-            .ForMember(dest => dest.SubstituteIngredients, opt => opt.MapFrom(src => src.SubstituteIngredients.Select(substitute => new SubstituteIngredient{OriginalIngredient = substitute, Substitute = substitute}).ToList()))
-            .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src =>src.Ingredients.Select(ingredient => new RecipeIngredient { Ingredient = new Ingredient { Name = ingredient.Name }, Quantity = ingredient.Quantity, UnitOfMeasure = (VeggieVibes.Domain.Enums.UnitOfMeasure)ingredient.UnitOfMeasure}).ToList()))
-            .ForMember(dest => dest.Category, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.RecipeCategory)src.Category))
-            .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.RecipeDifficulty)src.Difficulty))
-            .ForMember(dest => dest.DietType, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.DietType)src.DietType))
-            .ForMember(dest => dest.MealType, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.MealType)src.MealType))
-            .ForMember(dest => dest.Origin, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.CulinaryOrigin)src.Origin))
-            .ForMember(dest => dest.Allergen, opt => opt.MapFrom(src => (VeggieVibes.Domain.Enums.Allergen)src.Allergen));
+            .ForMember(dest => dest.SubstituteIngredients, opt => opt.MapFrom(src => src.SubstituteIngredients.Select(substitute => new SubstituteIngredient { OriginalIngredient = substitute, Substitute = substitute }).ToList()))
+            .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.Ingredients.Select(ingredient => new RecipeIngredient { Ingredient = new Ingredient { Name = ingredient.Name }, Quantity = ingredient.Quantity, UnitOfMeasure = (VeggieVibes.Domain.Enums.UnitOfMeasure)ingredient.UnitOfMeasure }).ToList()));
 
         CreateMap<RequestRegisterUserJson, User>()
-            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-            .ForMember(dest => dest.UserIdentifier, opt => opt.MapFrom(src => src.UserIdentifier))
-            .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+            .ForMember(dest => dest.Password, config => config.Ignore());
     }
 
     private void EntityToResponse()
@@ -52,7 +36,7 @@ public class AutoMapping : Profile
         CreateMap<Recipe, ResponseRecipeJson>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Instructions, opt => opt.MapFrom(src => src.Instructions.Select(instruction => instruction.Step))).ForMember(dest => dest.Variations, opt => opt.MapFrom(src => src.Variations.Select(variation => variation.Description))).ForMember(dest => dest.SubstituteIngredients, opt => opt.MapFrom(src => src.SubstituteIngredients.Select(substitute => substitute.Substitute))).ForMember(dest => dest.Allergen, opt => opt.MapFrom(src => src.Allergen)).ForMember(dest => dest.Tags, opt => opt.Ignore()).ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src =>src.Ingredients.Select(ingredient => new RequestRecipeIngredientsJson{IngredientId = ingredient.Ingredient.Id,Name = ingredient.Ingredient.Name, Quantity = ingredient.Quantity, UnitOfMeasure = (VeggieVibes.Communication.Enums.UnitOfMeasure)ingredient.UnitOfMeasure})));
-
+            
         CreateMap<User, ResponseUserJson>();
 
         CreateMap<Recipe, ResponseUpdateRecipeJson>();
