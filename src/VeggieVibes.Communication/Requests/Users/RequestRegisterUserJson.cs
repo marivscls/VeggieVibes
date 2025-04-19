65cac7fa3eabee7ade6b598778b860164d81a7ce
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-19 14:05:17 -0300

    removal of unnecessary properties

--- src/VeggieVibes.Communication/Requests/Users/RequestRegisterUserJson.cs
+++ src/VeggieVibes.Communication/Requests/Users/RequestRegisterUserJson.cs
@@ -5,6 +5,4 @@ public class RequestRegisterUserJson
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public Guid UserIdentifier { get; set; }
-    public string Role { get; set; } = string.Empty;
 }
--- src/VeggieVibes.Communication/Responses/Users/ResponseUserJson.cs
+++ src/VeggieVibes.Communication/Responses/Users/ResponseUserJson.cs
@@ -2,10 +2,7 @@ namespace VeggieVibes.Communication.Responses.Users;
 
 public class ResponseUserJson
 {
-    //public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public Guid UserIdentifier { get; set; }
-    public string Role { get; set; } = string.Empty;
 }
