7b087f8252332819fb912fe321a0906f663480aa
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-23 06:48:07 -0300

    add implement JWT token generation with user claims and expiration handling

---
+++ src/VeggieVibes.Domain/Security/Tokens/IAccessTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using VeggieVibes.Domain.Entities;
+
+namespace VeggieVibes.Domain.Security.Tokens
+{  
+    public interface IAccessTokenGenerator
+    {
+        string Generate(User user);
+    }
+}
---
+++ src/VeggieVibes.Infrastructure/Security/Token/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.WebSockets;
+using System.Security.Claims;
+using System.Text;
+using VeggieVibes.Domain.Entities;
+using VeggieVibes.Domain.Security.Tokens;
+
+namespace VeggieVibes.Infrastructure.Security.Token;
+public class JwtTokenGenerator : IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeInMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenGenerator(uint expirationTimeInMinutes, string signingKey)
+    {
+        _expirationTimeInMinutes = expirationTimeInMinutes;
+        _signingKey = signingKey;
+    }
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Sid, user.UserIdentifier.ToString()),
+        };
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeInMinutes),
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature),
+            Subject = new ClaimsIdentity(claims)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var  securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey SecurityKey()
+    {
+        var key = Encoding.UTF8.GetBytes(_signingKey);
+        return new SymmetricSecurityKey(key);
+    }
+}
+
