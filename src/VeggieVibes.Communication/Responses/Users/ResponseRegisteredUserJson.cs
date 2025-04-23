b166cf5a50704edb4b3ee81fe29af82a923b711f
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-23 09:56:11 -0300

    feat: update ResponseRegisteredUserJson to include additional fields for user registration response

---
+++ src/VeggieVibes.Communication/Responses/Users/ResponseRegisteredUserJson.cs
@@ -0,0 +1,9 @@
+namespace VeggieVibes.Communication.Responses.Users;
+
+public class ResponseRegisteredUserJson
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Token { get; set; } 
+}
--- src/VeggieVibes.Communication/Responses/Users/ResponseUserJson.cs
+++
@@ -1,8 +0,0 @@
-namespace VeggieVibes.Communication.Responses.Users;
-
-public class ResponseUserJson
-{
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
