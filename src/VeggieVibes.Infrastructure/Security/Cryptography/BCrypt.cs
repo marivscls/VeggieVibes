18d7706f655c594a6b5816d74a499fb4f0ad8641
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-25 18:50:09 -0300

    feat: implement BCrypt for password encryption and verification

--- src/VeggieVibes.Infrastructure/Security/Cryptography/BCrypt.cs
+++ src/VeggieVibes.Infrastructure/Security/Cryptography/BCrypt.cs
@@ -7,9 +7,13 @@ internal class BCrypt : IPasswordEncripter
 {
     public string Encrypt(string password)
     {
-        string passwordHash = BC.HashPassword(password);    
+        string passwordHash = BC.HashPassword(password);   
+
         return passwordHash;
     }
+
+    public bool Verify(string password, string passwordHash)
+    {
+        return BC.Verify(password, passwordHash);
+    }
 }
-
-
