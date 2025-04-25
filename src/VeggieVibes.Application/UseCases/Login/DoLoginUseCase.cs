22700b98c8a5f28c1562ee5458609fb3a82ac3b1
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-25 17:37:17 -0300

    add user login service and interface

---
+++ src/VeggieVibes.Application/UseCases/Login/DoLoginUseCase.cs
@@ -0,0 +1,31 @@
+﻿using VeggieVibes.Communication.Requests;
+using VeggieVibes.Communication.Responses.Users;
+using VeggieVibes.Domain.Repositories.Users;
+using VeggieVibes.Domain.Security.Cryptography;
+using VeggieVibes.Domain.Security.Tokens;
+
+namespace VeggieVibes.Application.UseCases.Login;
+
+public class DoLoginUseCase : IDoLoginUseCase
+{
+    private readonly IUserReadOnlyRepository _repository;
+    private readonly IPasswordEncripter _passwordEncripter;
+    private readonly IAccessTokenGenerator _accessTokenGenerator;
+
+    public DoLoginUseCase(IUserReadOnlyRepository repository, IPasswordEncripter passwordEncripter, IAccessTokenGenerator accessTokenGenerator)
+    {
+        _accessTokenGenerator = accessTokenGenerator;
+        _repository = repository;
+        _passwordEncripter = passwordEncripter;
+    }
+    public Task<ResponseRegisteredUserJson> Execute(RequestLoginJson request)
+    {
+        
+        return new ResponseRegisteredUserJson
+        {
+            
+        };
+    }
+}
+
+
---
+++ src/VeggieVibes.Application/UseCases/Login/IDoLoginUseCase.cs
@@ -0,0 +1,10 @@
+﻿using VeggieVibes.Communication.Requests;
+using VeggieVibes.Communication.Responses.Users;
+
+namespace VeggieVibes.Application.UseCases.Login;
+
+    public interface IDoLoginUseCase
+    {
+    Task<ResponseRegisteredUserJson> Execute(RequestLoginJson request);
+}
+
