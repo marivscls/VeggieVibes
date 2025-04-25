7ac0b63cb839c1bfe68090563d26af4c8dafaecf
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-25 17:37:34 -0300

    add user login controller

---
+++ src/VeggieVibes.Api/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VeggieVibes.Communication.Requests;
+using VeggieVibes.Communication.Responses.Users;
+
+namespace VeggieVibes.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController] 
+public class LoginController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login([FromServices] IDoLoginUseCase useCase, [FromBody] RequestLoginJson request)
+    {
+        var response = await useCase.Execute(request);
+        return Ok(response);
+    }
+}
+
