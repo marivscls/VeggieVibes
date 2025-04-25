47904ef912754e9dbe90e1e20ab595643781b961
MARIANA VASCONCELOS <mariana.vasconcelos@intactus.com.br>
2025-04-25 18:51:37 -0300

    feat: enhance UsersController with additional user management endpoints

--- src/VeggieVibes.Api/Controllers/LoginController.cs
+++ src/VeggieVibes.Api/Controllers/LoginController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using VeggieVibes.Application.UseCases.Login;
 using VeggieVibes.Communication.Requests;
 using VeggieVibes.Communication.Responses.Users;
 
